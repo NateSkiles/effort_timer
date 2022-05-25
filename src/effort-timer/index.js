735f6a8ff2e6a6ed68f670277f7e73e26d7f1b79
nateskiles <nateskiles94@gmail.com>
2022-05-25 09:34:46 -0700

    app init

---
+++ src/effort-timer/index.js
@@ -0,0 +1,17 @@
+const effortTimer = 'effort_timer';
+
+
+export default {
+ app: effortTimer,
+ title: 'Effort Timer',
+ version: '0.0.1',
+ dependencies: ['kustomer-^1.5.0'],
+ description: 'This app can be used to track the amount of time that an agent is putting effort into a conversation. Starting the timer indicates that you are actively working on the open conversation. Stopping the timer indicates that you have finished working on a conversation for now.',
+ appDetails: {
+   appDeveloper: {
+     name: 'Kustomer',
+     website: 'https://kustomer.com',
+     supportEmail: 'support@kustomer.com',
+   }
+ },
+};
