fab4f63e36820f76d7e6ffcb7bf4922d92f47873
nateskiles <nateskiles94@gmail.com>
2022-06-09 21:55:01 -0700

    major update

---
+++ app.js
@@ -0,0 +1,159 @@
+let attributes,
+    convoId,
+    running,
+    conversationTimerStartAt,
+    durationTotal = 0,
+    durationCurrent = 0,
+    startDate,
+    startTimer;
+
+const timerText = document.getElementById("timerText");
+
+
+async function loadContext() {
+    await Kustomer.initialize(function (contextJSON) {
+        attributes = contextJSON.conversation.attributes;
+        convoId = contextJSON.conversation.id;
+
+        if (attributes.custom['@effotim2TimerBool']) {
+            running = attributes.custom['@effotim2TimerBool'];
+        }
+
+        if (attributes.custom['@effotim2TimerStartAt']) {
+            startDate = new Date(attributes.custom['@effotim2TimerStartAt']).toISOString();
+        }
+
+        if (attributes.custom['@effotim2DurationNum']) {
+            durationTotal = attributes.custom['@effotim2DurationNum'];
+        }
+
+        // If timerBool is true, find current duration, set timer, then start set interval
+        // Else, set timer with current duration on conversation
+        if (running) {
+            let currentTime = new Date().toISOString();
+            durationCurrent = new Date(currentTime) - new Date(startDate) + durationTotal;
+
+            setTime(durationCurrent);
+
+            timerText.style.backgroundColor = "#ff4c55";
+            timerText.innerText = "STOP";
+
+            startTimer = setInterval(() => {
+                durationCurrent += 1000
+                setTime(durationCurrent)
+            }, 1000)
+
+        } else {
+            setTime(durationTotal);
+            timerText.style.backgroundColor = "#2ea44f";
+            timerText.innerText = "START";
+        }
+    });
+}
+
+async function timer() {
+    let currentTime = new Date().toISOString();
+
+    // If called while timer is running, find current time, then calculate difference between current time and recorded start
+    // Sum the duration timer was running with recorded duration on the conversation
+    if (running) {
+        let dateDiff = new Date(currentTime) - new Date(startDate);
+        durationTotal += dateDiff;
+
+        await Kustomer.request({
+            url: "/v1/conversations/" + convoId,
+            method: "PUT",
+            body: {
+                custom: {
+                    "@effotim2TimerBool": false,
+                    "@effotim2TimerEndAt": currentTime,
+                    "@effotim2DurationNum": durationTotal,
+                },
+            },
+        }, (err, conversations) => {
+            if (err || !conversations) {
+                console.log(err);
+                return;
+            }
+            else {
+                // Set timer with duration returned in response, stop timer (interval) and set running to false, update timer button
+                setTime(conversations.attributes.custom['@effotim2DurationNum']);
+                clearInterval(startTimer);
+                running = false;
+                timerText.style.backgroundColor = "#2ea44f";
+                timerText.innerText = "START";
+                return console.log("Stopping Timer");
+            }
+        });
+    } else {
+        // If timer wasn't running - set running to true and record timer start time
+        await Kustomer.request({
+            url: "/v1/conversations/" + convoId,
+            method: "PUT",
+            body: {
+                custom: {
+                    "@effotim2TimerBool": true,
+                    "@effotim2TimerStartAt": currentTime,
+                },
+            },
+        }, (err, conversations) => {
+            if (err || !conversations) {
+                console.log(err);
+                return;
+            }
+            else {
+                // startDate = conversations.attributes.custom.effotim2TimerStartAt; not needed?
+                if (conversations.attributes.custom['@effotim2DurationNum']) {
+                    durationCurrent = conversations.attributes.custom['@effotim2DurationNum']
+                };
+
+                running = true;
+                timerText.style.backgroundColor = "#ff4c55";
+                timerText.innerText = "STOP";
+
+                console.log(durationCurrent)
+
+                startTimer = setInterval(() => {
+                    durationCurrent += 1000
+                    setTime(durationCurrent)
+                }, 1000)
+
+                return console.log("Starting Timer");
+            }
+        });
+    }
+}
+
+// Utility Functions
+// Convert ms to time object
+function convertMiliseconds(miliseconds) {
+    let hours, minutes, seconds, total_minutes, total_seconds;
+
+    total_seconds = parseInt(Math.floor(miliseconds / 1000));
+    total_minutes = parseInt(Math.floor(total_seconds / 60));
+
+    seconds = parseInt(total_seconds % 60);
+    minutes = parseInt(total_minutes % 60);
+    hours = parseInt(Math.floor(total_minutes / 60));
+
+    return { h: hours, m: minutes, s: seconds };
+}
+
+// Set clock from time object
+function setTime(durationTotal) {
+    let durationCurrent = convertMiliseconds(durationTotal);
+    if (durationTotal) {
+        document.getElementById("hh").innerText = `${durationCurrent.h.pad()}`;
+        document.getElementById("mm").innerText = `${durationCurrent.m.pad()}`;
+        document.getElementById("ss").innerText = `${durationCurrent.s.pad()}`;
+    }
+}
+
+// Display leading zero
+Number.prototype.pad = function () {
+    let s = String(this);
+    if (s.length < 2) {
+        s = "0" + s;
+    }
+    return s;
+};
--- src/effort-timer-app/index.js
+++ src/effort-timer-app/index.js
@@ -1,11 +1,10 @@
 const effort_timer_app = 'effort_timer';
 
-
 export default {
   app: effort_timer_app,
   title: 'Effort Timer',
   iconUrl: "https://cdn.glitch.global/7ab9dfdc-9274-473d-a0a7-466d1abed72e/clock.png?v=1653500930639",
-  version: '0.0.2',
+  version: '0.1.0',
   dependencies: ['kustomer-^1.5.0'],
   description: 'This app can be used to track the amount of time that an agent is putting effort into a conversation. Starting the timer indicates that you are actively working on the open conversation. Stopping the timer indicates that you have finished working on a conversation for now.',
   appDetails: {
@@ -29,6 +28,30 @@ export default {
       }
     }
   },
+  klasses: [{
+    "name": "conversation",
+    "metadata": {
+      "standard": true,
+      "properties": {
+        "timerBool": {
+          "displayName": "Timer Boolean",
+          "example": "True/False"
+        },
+        "durationNum": {
+          "displayName": "Timer Duration",
+          "example": "Sum of timer duration"
+        },
+        "timerEndAt": {
+          "displayName": "Conversation Timer Start Time",
+          "example": ""
+        },
+        "timerStartAt": {
+          "displayName": "Conversation Timer Start Time",
+          "example": ""
+        },
+      }
+    }
+  }],
   i18n: {
     en_us: {
       [`${effort_timer_app}.settings.page.title`]: 'Effort Timer',
@@ -45,42 +68,40 @@ export default {
       type: 'rest_api',
       appSettings: {
         authToken: {
-          key: `${effort_timer_app}.default.authToken`
+          key: `effort_timer.default.authToken`
+        },
+        orgName: {
+          key: `effort_timer.default.orgName`
         }
       },
       inputTemplate: {
-        uri: '{{{orgName}}}.kustomerapp.com/v1/conversations/{{{conversationId}}}',
+        uri: 'https://{{orgName}}.api.kustomerapp.com/v1/conversations/{{conversationId}}',
         method: 'PUT',
         headers: {
-          authorization: 'Bearer {{{authToken}}}',
+          authorization: 'Bearer {{authToken}}',
           'Content-Type': 'application/json',
           Accept: 'application/json'
         },
         data: {
           custom: {
-            timerBool: false,
-            conversationTimerEndAt: '{{{currentTime}}}',
-            durationNum: '{{{durationNum}}}',
+            "@effotim2TimerBool": false,
+            "@effotim2TimerEndAt": '{{currentTime}}',
+            "@effotim2DurationNum": '/#durationNum',
           }
         },
         json: true
       },
       inputSchema: {
+        type: "object",
         properties: {
-          conversationId: {
-            type: 'string'
-          },
-          currentTime: {
-            type: 'string'
-          },
-          durationNum: {
-            type: 'number'
-          }
+          conversationId: { type: 'string' },
+          currentTime: { type: 'string' },
+          durationNum: { type: 'number' },
         },
         required: [
           'conversationId',
           'currentTime',
-          'durationNum'
+          'durationNum',
         ],
         additionalProperties: false
       },
@@ -114,4 +135,522 @@ export default {
       name: `kustomer.app.${effort_timer_app}.timer-card`
     }
   ],
+  workflows: [
+    {
+      "description": "",
+      "name": "effort-timer-1-of-4",
+      "steps": [
+        {
+          "transitions": [
+            {
+              "target": "3dvtbCHY4",
+              "condition": {
+                "op": "eq",
+                "values": [
+                  "/#steps.1.changes.attributes.custom.after.@effotim2TimerBool",
+                  true
+                ]
+              },
+              "meta": {
+                "name": "Timer Started"
+              }
+            }
+          ],
+          "errorCases": [],
+          "id": "DbjygSb7h"
+        },
+        {
+          "transitions": [],
+          "errorCases": [],
+          "id": "3dvtbCHY4",
+          "action": "kustomer.app.scheduler.schedule-workflow",
+          "params": {
+            "runAt": {
+              "value": 10,
+              "type": "relative"
+            },
+            "workflowId": "629806ce83f4a07ef11e0c76",
+            "inputs": {
+              "conversationId": "/#steps.1.id",
+              "@effotim2TimerStartAt": "/#steps.1.changes.attributes.custom.after.@effotim2TimerStartAt"
+            }
+          },
+          "appVersion": "scheduler-^1.0.1"
+        }
+      ],
+      "trigger": {
+        "transitions": [
+          {
+            "target": "DbjygSb7h",
+            "condition": {
+              "op": "or",
+              "values": [
+                {
+                  "op": "eq",
+                  "values": [
+                    "/#steps.1.changes.custom.attributes.before.@effotim2TimerBool",
+                    false
+                  ]
+                },
+                {
+                  "op": "dne",
+                  "values": [
+                    "/#steps.1.changes.attributes.custom.before.@effotim2TimerBool",
+                    true
+                  ]
+                }
+              ]
+            },
+            "meta": {
+              "name": "Timer Not Set"
+            }
+          }
+        ],
+        "eventName": "kustomer.conversation.update",
+        "id": "1",
+        "appVersion": "kustomer-^1.9.2"
+      }
+    },
+    {
+      "description": "",
+      "id": "629806ce83f4a07ef11e0c76",
+      "name": "effort-timer-30-minutes-2-of-4",
+      "steps": [
+        {
+          "transitions": [
+            {
+              "target": "ZjnrKj6AM",
+              "condition": {
+                "op": "true",
+                "values": [
+                  true
+                ]
+              }
+            }
+          ],
+          "errorCases": [],
+          "id": "BZZJOr1Jk",
+          "action": "kustomer.note.create",
+          "appVersion": "kustomer-^1.9.2",
+          "params": {
+            "conversation": "/#steps.1.conversationId",
+            "body": "Your timer has been running for 30 minutes."
+          }
+        },
+        {
+          "transitions": [
+            {
+              "target": "BZZJOr1Jk",
+              "condition": {
+                "op": "and",
+                "values": [
+                  {
+                    "op": "eq",
+                    "values": [
+                      "/#steps.DFtlTaetT.custom.@effotim2TimerBool",
+                      true
+                    ]
+                  },
+                  {
+                    "op": "eq",
+                    "values": [
+                      "/#steps.DFtlTaetT.custom.@effotim2TimerStartAt",
+                      "/#steps.1.@effotim2TimerStartAt"
+                    ]
+                  }
+                ]
+              },
+              "meta": {
+                "name": "Same Timer"
+              }
+            }
+          ],
+          "errorCases": [],
+          "id": "DFtlTaetT",
+          "action": "kustomer.conversation.find-by-id",
+          "params": {
+            "id": "/#steps.1.conversationId"
+          },
+          "appVersion": "kustomer-^1.9.2"
+        },
+        {
+          "transitions": [],
+          "errorCases": [],
+          "id": "ZjnrKj6AM",
+          "action": "kustomer.app.scheduler.schedule-workflow",
+          "params": {
+            "runAt": {
+              "value": 10,
+              "type": "relative"
+            },
+            "workflowId": "6298070dd1689c70089fb57e",
+            "inputs": {
+              "conversationId": "/#steps.1.conversationId",
+              "@effotim2TimerStartAt": "/#steps.1.@effotim2TimerStartAt"
+            }
+          },
+          "appVersion": "scheduler-^1.0.1"
+        }
+      ],
+      "trigger": {
+        "transitions": [
+          {
+            "target": "DFtlTaetT",
+            "condition": {
+              "op": "true",
+              "values": [
+                true
+              ]
+            }
+          }
+        ],
+        "id": "1",
+        "callable": true,
+        "schema": {
+          "properties": {
+            "conversationId": {
+              "type": "string"
+            },
+            "@effotim2TimerStartAt": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "conversationId",
+            "@effotim2TimerStartAt"
+          ]
+        },
+        "eventName": "kustomer.workflow.629806ce83f4a07ef11e0c76.call"
+      }
+    },
+    {
+      "description": "",
+      "id": "6298070dd1689c70089fb57e",
+      "name": "effort-timer-60-minutes-3-of-4",
+      "steps": [
+        {
+          "transitions": [
+            {
+              "target": "xEMM1c58l",
+              "condition": {
+                "op": "true",
+                "values": [
+                  true
+                ]
+              }
+            }
+          ],
+          "errorCases": [],
+          "id": "Yt1aQsVIT",
+          "appVersion": "kustomer-^1.9.2"
+        },
+        {
+          "transitions": [
+            {
+              "target": "iVeoA7IDx",
+              "condition": {
+                "op": "true",
+                "values": [
+                  true
+                ]
+              }
+            }
+          ],
+          "errorCases": [],
+          "id": "xEMM1c58l",
+          "action": "kustomer.note.create",
+          "params": {
+            "conversation": "/#steps.xRmOuTLR0.id",
+            "body": "Your timer has been running for 60 minutes."
+          },
+          "appVersion": "kustomer-^1.9.2"
+        },
+        {
+          "transitions": [
+            {
+              "target": "Yt1aQsVIT",
+              "condition": {
+                "op": "and",
+                "values": [
+                  {
+                    "op": "eq",
+                    "values": [
+                      "/#steps.xRmOuTLR0.custom.@effotim2TimerBool",
+                      true
+                    ]
+                  },
+                  {
+                    "op": "eq",
+                    "values": [
+                      "/#steps.xRmOuTLR0.custom.@effotim2TimerStartAt",
+                      "/#steps.1.@effotim2TimerStartAt"
+                    ]
+                  }
+                ]
+              },
+              "meta": {
+                "name": "Same Timer Check"
+              }
+            }
+          ],
+          "errorCases": [],
+          "id": "xRmOuTLR0",
+          "action": "kustomer.conversation.find-by-id",
+          "params": {
+            "id": "/#steps.1.conversationId"
+          },
+          "appVersion": "kustomer-^1.9.2"
+        },
+        {
+          "transitions": [],
+          "errorCases": [],
+          "id": "iVeoA7IDx",
+          "action": "kustomer.app.scheduler.schedule-workflow",
+          "params": {
+            "runAt": {
+              "value": 10,
+              "type": "relative"
+            },
+            "workflowId": "62980740565f5020cf3ab2dc",
+            "inputs": {
+              "conversationId": "/#steps.1.conversationId",
+              "effotim2TimerStartAt": "/#steps.1.@effotim2TimerStartAt"
+            }
+          },
+          "appVersion": "scheduler-^1.0.1"
+        }
+      ],
+      "trigger": {
+        "transitions": [
+          {
+            "target": "xRmOuTLR0",
+            "condition": {
+              "op": "true",
+              "values": [
+                true
+              ]
+            }
+          }
+        ],
+        "id": "1",
+        "callable": true,
+        "schema": {
+          "properties": {
+            "conversationId": {
+              "type": "string"
+            },
+            "@effotim2TimerStartAt": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "conversationId",
+            "@effotim2TimerStartAt"
+          ]
+        },
+        "eventName": "kustomer.workflow.6298070dd1689c70089fb57e.call"
+      }
+    },
+    {
+      "description": "",
+      "id": "62980740565f5020cf3ab2dc",
+      "name": "effort-timer-stop-timer-4-of-4",
+      "steps": [
+        {
+          "transitions": [
+            {
+              "target": "CKByP6iLV",
+              "condition": {
+                "op": "true",
+                "values": [
+                  true
+                ]
+              }
+            }
+          ],
+          "errorCases": [],
+          "id": "Z4L303Fyn",
+          "action": "kustomer.user.find-by-id",
+          "params": {
+            "id": "/#steps.SoXxw6grv.assignedUsers.0"
+          },
+          "appVersion": "kustomer-^1.9.2"
+        },
+        {
+          "transitions": [
+            {
+              "target": "anHK7NH4C",
+              "condition": {
+                "op": "true",
+                "values": [
+                  true
+                ]
+              }
+            }
+          ],
+          "errorCases": [],
+          "id": "CKByP6iLV",
+          "action": "kustomer.note.create",
+          "params": {
+            "conversation": "/#steps.SoXxw6grv.id",
+            "body": "**Stopping Timer**\n\nYour timer has been running for 4 hours, please restart timer if your you're still working on this conversation. ",
+            "userMentions": [
+              { "user": "/#steps.Z4L303Fyn.id" }
+            ]
+          },
+          "appVersion": "kustomer-^1.9.2"
+        },
+        {
+          "transitions": [
+            {
+              "target": "ibyRPoEGn",
+              "condition": {
+                "op": "true",
+                "values": [
+                  true
+                ]
+              }
+            }
+          ],
+          "errorCases": [],
+          "id": "anHK7NH4C",
+          "action": "kustomer.regex-match.generic",
+          "params": {
+            "testString": "{{{date 'hours' count=0}}}",
+            "regex": ".*"
+          },
+          "meta": {
+            "displayName": "Current Time"
+          },
+          "appVersion": "kustomer-^1.9.2"
+        },
+        {
+          "transitions": [
+            {
+              "target": "so7kOHPqH",
+              "condition": {
+                "op": "true",
+                "values": [
+                  true
+                ]
+              }
+            }
+          ],
+          "errorCases": [],
+          "id": "ibyRPoEGn",
+          "action": "kustomer.regex-match.generic",
+          "params": {
+            "testString": "{{dateDiff 'milliseconds' from=steps.SoXxw6grv.custom.@effotim2TimerStartAt to=steps.anHK7NH4C.match}}",
+            "regex": ".*"
+          },
+          "appVersion": "kustomer-^1.9.2",
+          "meta": {
+            "displayName": "Find Current Duration"
+          }
+        },
+        {
+          "transitions": [
+            {
+              "target": "SvQ1uaTpl",
+              "condition": {
+                "op": "true",
+                "values": [
+                  true
+                ]
+              }
+            }
+          ],
+          "errorCases": [],
+          "id": "so7kOHPqH",
+          "action": "kustomer.regex-match.generic",
+          "params": {
+            "testString": "{{ math steps.ibyRPoEGn.match \"+\" steps.SoXxw6grv.custom.@effotim2DurationNum }}",
+            "regex": ".*"
+          },
+          "meta": {
+            "displayName": "Sum Total Duration"
+          },
+          "appVersion": "kustomer-^1.9.2"
+        },
+        {
+          "transitions": [],
+          "errorCases": [],
+          "id": "SvQ1uaTpl",
+          "action": "kustomer.app.effort_timer_6142bf2f6ef1940524f53d94.put-request",
+          "params": {
+            "durationNum": "/#fn:parseInt,steps.so7kOHPqH.match",
+            "currentTime": "/#fn:dateFormat,steps.anHK7NH4C.match",
+            "conversationId": "/#steps.1.conversationId"
+          },
+          "appVersion": "effort_timer_6142bf2f6ef1940524f53d94-^0.0.30"
+        },
+        {
+          "transitions": [
+            {
+              "target": "Z4L303Fyn",
+              "condition": {
+                "op": "and",
+                "values": [
+                  {
+                    "op": "eq",
+                    "values": [
+                      "/#steps.SoXxw6grv.custom.@effotim2TimerBool",
+                      true
+                    ]
+                  },
+                  {
+                    "op": "eq",
+                    "values": [
+                      "/#steps.SoXxw6grv.custom.@effotim2TimerStartAt",
+                      "/#steps.1.@effotim2TimerStartAt"
+                    ]
+                  }
+                ]
+              },
+              "meta": {
+                "name": "Same Timer Check"
+              }
+            }
+          ],
+          "errorCases": [],
+          "id": "SoXxw6grv",
+          "action": "kustomer.conversation.find-by-id",
+          "params": {
+            "id": "/#steps.1.conversationId"
+          },
+          "appVersion": "kustomer-^1.9.2"
+        }
+      ],
+      "trigger": {
+        "transitions": [
+          {
+            "target": "SoXxw6grv",
+            "condition": {
+              "op": "true",
+              "values": [
+                true
+              ]
+            }
+          }
+        ],
+        "id": "1",
+        "callable": true,
+        "eventName": "kustomer.workflow.62980740565f5020cf3ab2dc.call",
+        "schema": {
+          "properties": {
+            "conversationId": {
+              "type": "string"
+            },
+            "@effotim2TimerStartAt": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "conversationId",
+            "@effotim2TimerStartAt"
+          ]
+        }
+      }
+    }
+  ]
 };
